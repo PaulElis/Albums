1a76d38dc58162211de290909190f3f7154f839a
github username <elis.paul7@gmail.com>
2018-08-19 12:11:27 -0400

    added album image

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text } from 'react-native'
 import AlbumDetail from './AlbumDetail'
 
 const URL = 'https://rallycoding.herokuapp.com/api/music_albums'
